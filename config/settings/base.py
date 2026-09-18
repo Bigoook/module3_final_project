dd6e11dab621b0dcc4f3830d26e66b7b8c619f8c
Oleh <bigoook@gmail.com>
2026-09-18 10:52:34 +0300

    chore(settings): add Pillow, LocaleMiddleware and i18n paths
    - Add Pillow to requirements for ImageField support
    - Add LocaleMiddleware for future language switching
    - Add LOCALE_PATHS and create the locale directory

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -96,6 +97,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+LOCALE_PATHS = [BASE_DIR / 'locale']
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
