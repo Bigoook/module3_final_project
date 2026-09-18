67d7c0752be103c555549d92a1f34a00bbd18af4
Oleh <bigoook@gmail.com>
2026-09-18 11:05:13 +0300

    feat(catalog): add Category model

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,1 +1,25 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    name = models.CharField(_('name'), max_length=150)
+    slug = models.SlugField(_('slug'), max_length=200, unique=True)
+    parent = models.ForeignKey(
+        'self',
+        verbose_name=_('parent category'),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='children',
+    )
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+    def __str__(self) -> str:
+        return self.name
