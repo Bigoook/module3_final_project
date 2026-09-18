6fb1da3f2762776f65adf8648c9e6538ac9323ff
Oleh <bigoook@gmail.com>
2026-09-18 11:22:35 +0300

    feat(catalog): add Product model

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -23,3 +23,29 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(_('name'), max_length=200)
+    slug = models.SlugField(_('slug'), max_length=250, unique=True)
+    description = models.TextField(_('description'))
+    price = models.DecimalField(_('price'), max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category,
+        verbose_name=_('category'),
+        on_delete=models.PROTECT,
+        related_name='products',
+    )
+    image = models.ImageField(_('image'), upload_to='products/', blank=True)
+    is_active = models.BooleanField(_('is active'), default=True)
+    stock = models.IntegerField(_('stock'), default=0)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+    def __str__(self) -> str:
+        return self.name
